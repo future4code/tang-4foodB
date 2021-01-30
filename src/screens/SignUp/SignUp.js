20250d1b9c31b8b079113636c39388684b0b404d
Paula Soares <sou@paulasoares.me>
2021-01-30 18:29:41 -0300

    fix router

--- src/screens/SignUp/SignUp.js
+++ src/screens/SignUp/SignUp.js
@@ -14,76 +14,77 @@ export const SignUp = () => {
         email: '',
         cpf: '',
         password: '',
-        confirmPassword: '',       
-});
+        confirmPassword: '',
+    });
 
-//const onSubmitForm = () => {
-//importar form do material e fazer validação de cpf e confirmação de senha
-//git}
+    const onSubmitForm = () => {
+        //importar form do material e fazer validação de cpf e confirmação de senha
+        console.log(form);
+        goToAddress(history);
+    }
     return (
         <SignUpContainer>
             <Logo src={logo}></Logo>
             <h3>Cadastrar</h3>
             <form id={"signup_form"} onSubmit={onSubmitForm}>
                 <FormContainer>
-                    <TextField required 
-                    id="name" 
-                    name="name" 
-                    label="Nome"
-                    type="name"
-                    variant="outlined" 
-                    placeholder="Nome e Sobrenome"
-                    onChange= {onChangeForm}
-                    value={form.name}/>
-                    <br/>
+                    <TextField required
+                        id="name"
+                        name="name"
+                        label="Nome"
+                        type="name"
+                        variant="outlined"
+                        placeholder="Nome e Sobrenome"
+                        onChange={onChangeForm}
+                        value={form.name} />
+                    <br />
 
-                    <TextField required 
-                    id="email" 
-                    name="email" 
-                    label="E-mail" 
-                    type="email" 
-                    variant="outlined"
-                    placeholder="email@email.com" 
-                    onChange= {onChangeForm}
-                    value={form.email}/>
-                    <br/>
+                    <TextField required
+                        id="email"
+                        name="email"
+                        label="E-mail"
+                        type="email"
+                        variant="outlined"
+                        placeholder="email@email.com"
+                        onChange={onChangeForm}
+                        value={form.email} />
+                    <br />
 
-                    <TextField required 
-                    id="cpf" 
-                    name="cpf" 
-                    label="CPF" 
-                    type="number"
-                    variant="outlined" 
-                    placeholder="000.000.000-00"
-                    onChange= {onChangeForm}
-                    value={form.cpf}/>
-                    <br/>
+                    <TextField required
+                        id="cpf"
+                        name="cpf"
+                        label="CPF"
+                        type="number"
+                        variant="outlined"
+                        placeholder="000.000.000-00"
+                        onChange={onChangeForm}
+                        value={form.cpf} />
+                    <br />
 
-                    <TextField required 
-                    id="password" 
-                    name="password" 
-                    label="Senha" 
-                    type="password"
-                    variant="outlined" 
-                    placeholder="Mínimo 6 caracteres"
-                    onChange= {onChangeForm}
-                    value={form.password}/>
-                    <br/>
+                    <TextField required
+                        id="password"
+                        name="password"
+                        label="Senha"
+                        type="password"
+                        variant="outlined"
+                        placeholder="Mínimo 6 caracteres"
+                        onChange={onChangeForm}
+                        value={form.password} />
+                    <br />
 
-                    <TextField required 
-                    id="confirmPassword" 
-                    name="confirmPassword" 
-                    label="Confirmar Senha" 
-                    type="password"
-                    variant="outlined" 
-                    placeholder="Confirme a senha anterior"
-                    onChange= {onChangeForm}
-                    value={form.confirmPassword}/>
+                    <TextField required
+                        id="confirmPassword"
+                        name="confirmPassword"
+                        label="Confirmar Senha"
+                        type="password"
+                        variant="outlined"
+                        placeholder="Confirme a senha anterior"
+                        onChange={onChangeForm}
+                        value={form.confirmPassword} />
 
                 </FormContainer>
+                <Button> Criar</Button>
             </form>
-            {/* <Button onClick={()=> goToAddress(history)}> Criar</Button> */}
-            <Button onClick={()=> console.log(form)}> Criar</Button>
         </SignUpContainer>
     )
 }
