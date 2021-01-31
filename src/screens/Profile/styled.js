c0087db738ce1374e4eab6ed024fb822a330c54c
Bruna Pacheco <brunappacheco@hotmail.com>
2021-01-31 17:20:54 -0400

    pagina profile ok

--- src/screens/Profile/styled.js
+++ src/screens/Profile/styled.js
@@ -21,10 +21,10 @@ export const TitlePerfil = styled.div`
 width: 22.25rem;
 height: 2.8rem;
 font-family: Roboto;
-font-size: 1rem;
+font-size: 20px;
 text-align: center;
 margin: 1.25rem 5.75rem 0 5.813rem;
-padding: 0.813rem 3.344rem 0.75rem;
+
 `
 export const DataUser = styled.div`
 width: 20.5rem;
@@ -37,10 +37,11 @@ width: 1.5rem;
 height: 1.5rem;
 margin: 1rem 1rem 0.125rem 20rem;
 object-fit: contain;
+margin-top: -35px;
 `
 export const RegisterAdress = styled.div`
 width: 120%;
-margin-top: 5rem;
+margin-top: -5rem;
 margin-left: 6rem;
 margin-bottom: 100px;
 padding: 1rem;
@@ -57,7 +58,7 @@ object-fit: contain;
 export const Requests = styled.div`
 width: 20.5rem;
 height: 1.125rem;
-margin-bottom: 1rem;
+margin-top:5rem;
 font-family: Roboto;
 font-size: 1rem;
 font-weight: normal;
@@ -67,9 +68,16 @@ line-height: normal;
 letter-spacing: -0.39px;
 color: var(--black);
 `
+export const TextRequest = styled.p`
+margin-top: -10rem;
+`
+
 export const LineBlack = styled.div`
-width: 20.5rem;
-margin: 0.5rem 1rem 1rem;
-border: solid 0.1px black;
-  
-  `
+width: 23.5rem;
+margin-left: 5vh;
+margin-right: -1vh;
+border: solid 0.1px black; 
+`
+export const MessageProfile = styled.p`
+margin-top: 10px;
+`
