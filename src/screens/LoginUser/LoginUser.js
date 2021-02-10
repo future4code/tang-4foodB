3d78ee30c3127325a3de5bdadcadd0a05e6c5ada
Bruna Pacheco <brunappacheco@hotmail.com>
2021-02-10 18:52:55 -0400

    FormControl password visibility/sisibilityoff na pagina de login

--- src/screens/LoginUser/LoginUser.js
+++ src/screens/LoginUser/LoginUser.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useHistory } from "react-router-dom";
 import logo from '../../img/logo.png';
 import { FormContainer, LoginContainer, Logo, Button } from './styled';
@@ -7,19 +7,59 @@ import useForm from '../../hooks/useForm';
 import { goToSignup } from "../../routes/Coordinator";
 import {UrlApi} from '../../constants/urls'
 import axios from 'axios';
+// fiz a instalação do loading usando o ROLLER, porém ainda falta função//;
+import { Roller } from "react-awesome-spinners";
+//password imports abaixo//
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+  
 
+// - password estilização - //;
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    margin: {
+      margin: theme.spacing(1),
+    },
+    withoutLabel: {
+      marginTop: theme.spacing(3),
+    },
+    textField: {
+      width: '40ch',
+    },
+  }));
+
+// -----*--- fim password estilização----*--- //
+  
 export const LoginUser = () => {
     const history = useHistory();
+//aqui sou eu de novo fazendo o password funcionar//
+    const classes = useStyles();
+    const [values, setValues] = React.useState({ 
+        password: '',
+    showPassword: false,
+  });
+// --------*----pronto-------*-------- //
+
+    const [loading, setLoading] = useState(true);
     const [form, onChangeForm] = useForm({
-        email: '',
-        password: '',      
+        email: '',   
 });
 
     const login = (event) => {
         event.preventDefault()
         const body = {
             email: form.email,
-            password: form.password
+    
           };
       
           axios
@@ -36,14 +76,30 @@ export const LoginUser = () => {
               console.log(error)
               });
     }
+    //bruna passando outra vez //
+    const handleChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+      };
+    
+      const handleClickShowPassword = () => {
+        setValues({ ...values, showPassword: !values.showPassword });
+      };
+    
+      const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+      };
+    // terminando aqui de novo ///
     
 
     return (
         <LoginContainer>
             <Logo src={logo}></Logo>
             <h3>Entrar</h3>
+            <br>
+            </br>
             <form id={"login_form"} onSubmit={login}>
                 <FormContainer>
+                    
                     <TextField 
                         name={"email"}
                         required 
@@ -54,20 +110,49 @@ export const LoginUser = () => {
                         onChange={onChangeForm} 
                         value={form.email} />
                     <br />
-                    <TextField required 
-                        name={"password"}
-                        id="password" 
-                        label="Senha" 
-                        type="password" 
-                        placeholder="Mínimo 6 caracteres" 
-                        variant="outlined" 
-                        onChange={onChangeForm} 
-                        value={form.password}/>
-                    <Button>Entrar</Button>
-                </FormContainer>
+            <div
+             className={classes.root}>
+            <div>
+
+                <FormControl className={clsx(classes.margin, classes.textField)} variant="outlined">
+                  <InputLabel htmlFor="outlined-adornment-password">
+                    Senha
+                  </InputLabel>
+                <OutlinedInput
+                     id="outlined-adornment-password"
+                     type={values.showPassword ? 'text' : 'password'}
+                     value={values.password}
+                     onChange={handleChange('password')}
+                     endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+                </InputAdornment>
+                    }
+                    labelWidth={70}
+                />
+                </FormControl>
+        
+            </div>
+            </div>
+
+                <Button>
+                 Entrar
+                </Button>
+            </FormContainer>
+
             </form>
+             
+            <Roller/>
             <p>Não possui cadastro?</p>
             <p onClick={()=> goToSignup(history)}> Clique aqui.</p>
+        
         </LoginContainer>
     )
 }
